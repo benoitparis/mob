1245f4e15ab23e296d6444533f845fc8c77efaac
Benoît Paris <benoit.paris.753@gmail.com>
2020-02-26 13:14:25 +0100

    tick: fixed flooding the client

--- apps/tick/sql/1_ack_value.update.sql
+++ apps/tick/sql/1_ack_value.update.sql
@@ -6,4 +6,9 @@ SELECT
     CAST(ts.tick_number AS VARCHAR)
   )
 FROM write_value AS wv
-JOIN services.tick AS ts ON true
+JOIN (
+  SELECT
+    LAST_VALUE(tick_number) tick_number
+  FROM services.tick 
+) AS ts 
+  ON true
